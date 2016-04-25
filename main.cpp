bf5374153d194d0adec7db0e92b432b1ba475017
ashancoc <admin@macbook-pro.mobile.asu.edu>
2016-04-25 14:20:44 -0700

    Cleared my duplication mistakes in main

--- main.cpp
+++ main.cpp
@@ -10,17 +10,11 @@ int main();				// The main function
 void menu();			// display main choices
 void branching(char);	// branch to the chosen function
 
-//not sure if my void/int clarifiers are correct... been working on the inside of the functions mainly.
-void recommendationsList();
-int addToCatalog();
-int removeFromCatalog();
-int addToShoppingCart();
-int checkOut();
 
 //=====================================================================================
 // function addToCatalog()
 //=====================================================================================
-int addToCatalog(){
+void addToCatalog(){
 	//Not sure if I have to redeclare these... as I was working on getting this, I tried to access Book
 	//To do something like cin.getline(Book::title, 50), etc. etc. but I followed the similar style
 	//from the previous assignment.
@@ -49,13 +43,22 @@ int addToCatalog(){
 	if (strcmp(bookType, "Textbook") ==0) category = TEXTBOOK;
 	else if (strcmp(bookType, "Fiction") ==0) category = FICTION;
 	else if (strcmp(bookType, "Magazine") ==0) category = MAGAZINE;
+	else if (strcmp(bookType, "Book") ==0) category = BOOK;
 	else{
 		cout <<"Invalid input for bookType" << endl;
 		//call a break statement or a loop of some sort to have it redo the entry for category???
 	}
 
 	switch (category) {
-		case TEXTBOOK: cout<<"BookType is TEXTBOOK."<<endl;
+		case TEXTBOOK: {
+			cout<<"BookType is TEXTBOOK."<<endl;
+			//Uncertain on how to do this but almost feel that under this have it then
+			//do the construction of Textbook(bookID,title,price,category)
+
+		}
+		case BOOK: {
+			cout<<"BookType is BOOK."<<endl;
+		}
 		case FICTION: {
 			cout<<"BookType is FICTION."<<endl;
 			cout<<"Please enter name of author: "<<endl;
@@ -148,42 +151,4 @@ int main(int argc, char *argv[]) {
 	}
 	while (ch != 'q');			// 'q' for quit
 	return 0;
-}
-
-//=====================================================================================
-// function menu()
-//=====================================================================================
-void menu() {
-	cout <<	endl << endl << "MENU" << endl;
-	cout << "----" << endl;
-	cout << "s: See our Recommendations!" << endl;
-	cout << "a: Add a book to the catalog." << endl;
-	cout << "r: Remove a book from the catalog." << endl;
-	cout << "b: Add books to the shopping cart." << endl;
-	cout << "f: Proceed to check out."<<endl;
-	cout << "q: Exit the store." << endl;
-	cout << endl << "Please enter your choice () --> ";
-}
-
-//=====================================================================================
-// function branching()
-//=====================================================================================
-void branching(char c) {		// branch to chosen function
-	switch(c) {
-		case 's':	recommendationsList();
-			break;
-		case 'a':	addToCatalog();
-			break;
-		case 'r':	removeFromCatalog();
-			break;
-		case 'b':	addToShoppingCart();
-			break;
-		case 'f':	checkOut();
-			break;
-		case 'q':	cout << endl << "@Exiting the program..............." << endl;
-			cin.get();	//type any key.
-			break;
-		default:	cout << endl << "@ERROR - Invalid input." << endl;
-			cout << "@Try again....." << endl;
-	}
 }
